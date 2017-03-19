0ef0a967b51acc1931010082d813e0885876f882
Albrecht Noll <albrecht.noll@grandcentrix.net>
2017-03-19 18:17:04 +0100

    solution excercise android basics

--- hello_world/app/src/main/java/de/fhws/helloworld/MainActivity.java
+++ hello_world/app/src/main/java/de/fhws/helloworld/MainActivity.java
@@ -2,18 +2,27 @@ package de.fhws.helloworld;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView)findViewById(R.id.myTextView);
-
-        //tv.setText( "Hello World" ); // not good!
-        tv.setText( R.string.helloworld ); // better!
+        final TextView textView = (TextView) findViewById(R.id.myTextView);
+        final Button button = (Button) findViewById(R.id.myButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                mCount++;
+                textView.setText(getString(R.string.text_counter, mCount));
+            }
+        });
     }
 }
