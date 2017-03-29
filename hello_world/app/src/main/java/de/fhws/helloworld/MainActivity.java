862475fbf94afb61a8ca3f0e8ed28940cdb35d31
Albrecht Noll <albrecht.noll@grandcentrix.net>
2017-03-29 18:29:45 +0200

    add intent examples

--- hello_world/app/src/main/java/de/fhws/helloworld/MainActivity.java
+++ hello_world/app/src/main/java/de/fhws/helloworld/MainActivity.java
@@ -1,28 +1,36 @@
 package de.fhws.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int mCount = 0;
+    @Override
+    public void onClick(final View v) {
+        switch (v.getId()) {
+            case R.id.explicit:
+                final Intent intent = new Intent(this, SecondActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.implicit:
+                final Intent implicit = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(implicit);
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.myTextView);
-        final Button button = (Button) findViewById(R.id.myButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                mCount++;
-                textView.setText(getString(R.string.text_counter, mCount));
-            }
-        });
+        final Button explicitButton = (Button) findViewById(R.id.explicit);
+        final Button implicitButton = (Button) findViewById(R.id.implicit);
+        explicitButton.setOnClickListener(this);
+        implicitButton.setOnClickListener(this);
     }
 }
