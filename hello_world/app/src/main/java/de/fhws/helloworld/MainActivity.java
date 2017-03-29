0e005757154d13307d3121ff0a452bb92165f727
Albrecht Noll <albrecht.noll@grandcentrix.net>
2017-03-29 18:53:09 +0200

    solution excercise receive intent

--- hello_world/app/src/main/java/de/fhws/helloworld/MainActivity.java
+++ hello_world/app/src/main/java/de/fhws/helloworld/MainActivity.java
@@ -6,14 +6,18 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mInputView;
 
     @Override
     public void onClick(final View v) {
         switch (v.getId()) {
             case R.id.explicit:
                 final Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("userinput", mInputView.getText().toString());
                 startActivity(intent);
                 break;
             case R.id.implicit:
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInputView = (EditText) findViewById(R.id.editText);
 
         final Button explicitButton = (Button) findViewById(R.id.explicit);
         final Button implicitButton = (Button) findViewById(R.id.implicit);
--- hello_world/app/src/main/java/de/fhws/helloworld/SecondActivity.java
+++ hello_world/app/src/main/java/de/fhws/helloworld/SecondActivity.java
@@ -1,11 +1,9 @@
 package de.fhws.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -16,6 +14,10 @@ public class SecondActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        final String text = intent.getExtras().getString("userinput", "empty");
+        getSupportActionBar().setTitle(text);
     }
 
 }
