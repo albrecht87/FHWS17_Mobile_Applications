743253587665fe39d1a7a13cd5b01b10afe2f352
Albrecht Noll <albrecht.noll@grandcentrix.net>
2017-04-05 18:39:21 +0200

    added creation log

--- hello_world/app/src/main/java/de/fhws/helloworld/recyclerview/Adapter.java
+++ hello_world/app/src/main/java/de/fhws/helloworld/recyclerview/Adapter.java
@@ -4,6 +4,7 @@ package de.fhws.helloworld.recyclerview;
 import com.squareup.picasso.Picasso;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,22 +17,20 @@ import de.fhws.helloworld.R;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder {
 
         final ImageView mImageView;
 
         final TextView mTitle;
 
-        public ViewHolder(final View itemView) {
+        ViewHolder(final View itemView) {
             super(itemView);
             mTitle = (TextView) itemView.findViewById(R.id.headline);
             mImageView = (ImageView) itemView.findViewById(R.id.card_image);
         }
-
-        public void setData(String title) {
-            mTitle.setText(title);
-        }
     }
+
+    private static final String TAG = Adapter.class.getSimpleName();
 
     private ArrayList<String> mData;
 
@@ -49,17 +48,13 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         holder.mTitle.setText(mData.get(position));
         final Picasso picasso = Picasso.with(holder.mImageView.getContext());
         picasso.setLoggingEnabled(true);
-        //picasso.cancelRequest(holder.mImageView);
-        //picasso.invalidate("https://source.unsplash.com/random/800x600&raandom="+position);
         picasso.load("https://source.unsplash.com/random/800x600&raandom="+position)
                 .stableKey(""+position)
-                //.networkPolicy(NetworkPolicy.NO_STORE, NetworkPolicy.NO_CACHE)
-                //.memoryPolicy(MemoryPolicy.NO_STORE, MemoryPolicy.NO_CACHE)
                 .into(holder.mImageView);
     }
-
     @Override
     public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+        Log.d(TAG, "onCreateViewHolder");
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.list_item_card, parent, false);
         return new ViewHolder(view);
